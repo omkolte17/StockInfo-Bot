26d486426770db1a0cc4ade3c1e9fbd78f6debb7
Om Kolte <omkolte17@gmail.com>
2022-07-14 10:57:58 +0530

    optimize

--- bot.py
+++ bot.py
@@ -1,55 +1,52 @@
 from telethon import events, TelegramClient
 from yahoo_fin import stock_info as si
+import traceback
 
 # Initialize TelegramClient
-API_ID = "API ID HERE"
-API_HASH = "API HASH HERE"
+API_ID = "API_ID HERE"      # remove the quotes for API_ID
+API_HASH = "API_HASH HERE"
+start_msg = "STONKS! \nPress /help for more info"
+command_msg = "Commands: \n\t\t/getval - Returns Current Price \n\t\t/getholders - Returns Major Holders"
 
-client = TelegramClient('stocksinfo', API_ID, API_HASH).start(
-    bot_token='bot token here')
+client = TelegramClient('stocksinfo', API_ID, API_HASH).start(bot_token='bot_token here')
+
 
 # Handlers
-
-
 @client.on(events.NewMessage(pattern="/start"))
 async def handle_start(e):
-    await e.reply("STONKS\n"+"Press /help for more information.")
+    await e.reply(start_msg)
 
 
 @client.on(events.NewMessage(pattern="/help"))
 async def handle_help(e):
-    await e.reply("Commands:\nGet Current Price - /getval <tickername>\n" +
-                  "Get Major Holders - /getholders")
+    await e.reply(command_msg)
 
 
 @client.on(events.NewMessage(pattern="/getval"))
 async def handle_getval(e):
-    tickerPrice = e.raw_text+".NS"
-    tickerPrice = tickerPrice.split(" ")
-    tickerPrice = tickerPrice[1]
+    ticker = e.raw_text+".NS"
+    ticker = ticker.split(" ")[1]
     price = None
     try:
-        price = si.get_live_price(tickerPrice)
+        price = si.get_live_price(ticker)
         await e.reply("Current Price: {:.2f}".format(price))
     except:
         await e.reply("Enter valid ticker.")
-        print("/getval error")
+        traceback.print_exc()
 
 
 @client.on(events.NewMessage(pattern="/getholders"))
 async def handle_getholders(e):
-    tickerHoldings = e.raw_text+".NS"
-    tickerHoldings = tickerHoldings.split(" ")
-    tickerHoldings = tickerHoldings[1]
+    ticker = e.raw_text+".NS"
+    ticker = ticker.split(" ")[1]
     holdings = None
     try:
-        holdings = si.get_holders(tickerHoldings)
-        await e.reply("Major Holders of {} are as follow:\n".format(tickerHoldings)
+        holdings = si.get_holders(ticker)
+        await e.reply("Major Holders of {} are as follow:\n".format(ticker)
                       + str(holdings['Direct Holders (Forms 3 and 4)']
                             ['Holder']))
     except:
         await e.reply("Enter valid ticker.")
-        print("/getholders error")
 
 # Start Client
 
